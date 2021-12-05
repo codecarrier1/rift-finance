9a40ce200aad25e44638eb6b3ffafc69a181397d
topdev17 <dannyjin117@gmail.com>
2021-12-05 09:09:19 -0500

    WIP option card

--- src/Body.tsx
+++ src/Body.tsx
@@ -3,10 +3,30 @@ import { Balance, OptionCard, ProgressDays } from './components';
 
 import { options } from './constants';
 
+const initialUserState: { [key: string]: {} } = {
+  COMPOUND: {
+    deposited: 0,
+    interest: 0,
+  },
+  AAVE: {
+    deposited: 0,
+    interest: 0,
+  },
+  CURVE: {
+    deposited: 0,
+    interest: 0,
+  },
+};
+
 const Body: React.FC = () => {
   const [balance, setBalance] = useState(1000);
+  const [userState, setUserState] = useState(initialUserState);
 
   const handleProgressDays = (days: number) => {};
+
+  const handleDeposit = (network: string, amount: number) => {};
+
+  const handleWithdraw = (network: string) => {};
 
   return (
     <div id='body' className='px-6 py-4 flex-grow max-w-7xl w-full m-auto'>
--- src/components/optionCard/optionCard.component.tsx
+++ src/components/optionCard/optionCard.component.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export const OptionCard = () => {
-  return <div></div>;
+  return (
+    <div className='px-4 py-3 text-white text-center border-2 border-white'></div>
+  );
 };
