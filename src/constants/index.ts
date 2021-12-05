226f1a94ae402ec8ccc15fa01daac0d2e163b50f
topdev17 <dannyjin117@gmail.com>
2021-12-05 08:29:36 -0500

    add option types

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,23 @@
-export const options = [];
+type IOption = {
+  name: string;
+  apy: number;
+  type: 'COMPOUND' | 'AVAE' | 'CURVE';
+};
+
+export const options: IOption[] = [
+  {
+    name: 'Compound',
+    apy: 5,
+    type: 'COMPOUND',
+  },
+  {
+    name: 'Aave',
+    apy: 3,
+    type: 'AVAE',
+  },
+  {
+    name: 'Curve',
+    apy: 2.5,
+    type: 'CURVE',
+  },
+];
